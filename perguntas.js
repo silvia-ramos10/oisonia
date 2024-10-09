097955f872504b00631430d22c712b2d6cb467f5
silvia-ramos10 <silviasrmc@gmail.com>
2024-10-09 11:37:59 -0300

    k.bj.

---
+++ perguntas.js
@@ -0,0 +1,100 @@
+criaCartao (
+    'Biologia',
+    'Qual é o nome da estrutura que é o órgão sensorial termorreceptor presente na família das serpentes?',
+    'Fosseta loreal'
+    )
+    criaCartao(
+    'Biologia',
+    'O GAFANHOTO É VERDE PORQUE VIVE NA GRAMA Qual evolucionista está relacionadoa essa afirmação?',
+    'Lamarck'
+    )
+    criaCartao(
+    'Biologia',
+    'Qual é o Filo das flores? Exemplo: Margarida',
+    'Angiosperma'
+    )
+    criaCartao(
+    'Matemática',
+    'Qual é a fórmula de bhaskara?',
+    'X= -b±√b²-4ac/2a'
+    )
+    criaCartao(
+    'Matemática',
+    'Como calcular o perímetro de um triângulo?',
+    'Basta somar todos os lados do triângulo'
+    )
+    criaCartao(
+    'Matemática',
+    'Como transformar graus em radianos?',
+    'Basta multiplicar o grau com o pi/180º'
+    )
+    criaCartao(
+    'Filosofia',
+    'Qual é a frase mais famosa de aristóteles?',
+    'O sábio nunca diz tudo o que pensa, mas pensa tudo o que diz'
+    )
+    criaCartao(
+    'Filosofia',
+    'O que é a teoria do contrato social?',
+    'A teoria do contrato social explora a origem e a justificativa da autoridade política e da organização social através da ideia de um acordo hipotético ou real entre os indivíduos para formar uma sociedade organizada'
+    )
+    criaCartao(
+    'Filosofia',
+    'Qual é a diferença entre o empirismo e o racionalismo?',
+    'Ambos os enfoques influenciam nossa compreensão do conhecimento ao destacar diferentes fontes e métodos para obter verdades sobre o mundo. O empirismo tende a enfatizar a importância da observação e da experiência direta, enquanto o racionalismo valoriza o poder da razão e da dedução lógica'
+    )
+    criaCartao(
+    'Inglês',
+    'Quais pronomes pessoais que usam are no verb to be?',
+    'You, We e They'
+    )
+    criaCartao(
+    'Inglês',
+    'O que é present continuous?',
+    'Esse tempo verbal deve ser usado para expressar uma situação que está em progresso, ou seja, uma ação que está em acontecendo no momento da fala'
+    )
+    criaCartao(
+    'Inglês',
+    'Como a diferença entre os tempos verbais presente perfeito e passado simples pode alterar o significado de uma frase em inglês?',
+    'A principal diferença é que o presente perfeito conecta o passado com o presente, enquanto o passado simples refere-se a ações que são totalmente concluídas no passado'
+    )
+    criaCartao(
+    'Geografia',
+    'Qual é a maior potência do mundo?',
+    'Estados Unidos'
+    )
+    criaCartao(
+    'Geografia',
+    'Em qual continente fica o rio mais largo do mundo?',
+    'América do Sul'
+    )
+    criaCartao(
+    'Geografia',
+    'Qual é o rio mais largo do mundo?',
+    'Rio Amazonas'
+    )
+    criaCartao(
+    'História',
+    'Quais foram as principais razões para o afastamento de Getúlio Vargas da presidência em 1945?',
+    'Getúlio Vargas foi afastado da presidência em 1945 devido a uma combinação de fatores políticos e militares'
+    )
+    criaCartao (
+    'História',
+    'Qual foi o papel de Getúlio Vargas no processo de modernização e industrialização do Brasil durante seu governo?',
+    'Ele implementou políticas de substituição de importações, apoiou a criação de empresas estatais, como a Companhia Siderúrgica Nacional e a Petrobras'
+    )
+    criaCartao (
+    'História',
+    'Como a Constituição de 1934, promulgada durante o governo de Vargas, influenciou o sistema político brasileiro?',
+    'A Constituição de 1934, promulgada durante o governo de Getúlio Vargas, introduziu importantes reformas no sistema político brasileiro'
+    )
+    criaCartao (
+    'Português',
+    'Como a técnica do realismo psicológico em obras de Machado de Assis?',
+    'O realismo psicológico, estilo do qual a obra em questão é representante, refere-se à preferência do narrador pela descrição psicológica das personagens, em detrimento da ênfase na ação, no enredo, nas complicações'
+    )
+    criaCartao (
+    'Português',
+    'Qual foi o impacto de Machado de Assis na literatura brasileira?',
+    'Ele foi fundador e primeiro presidente da Academia Brasileira de Letras, além de ter escrito alguns dos maiores clássicos da literatura brasileira'
+    )
