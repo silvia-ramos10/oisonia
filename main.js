17f1e2a6ef7ea0df756467afcd2afb54cf5011d5
silvia-ramos10 <silviasrmc@gmail.com>
2024-10-09 12:02:06 -0300

    x

--- main.js
+++ main.js
@@ -3,6 +3,14 @@ let container = document.getElementById('container');
 let cartao = document.createElement('article');
 cartao.className = 'cartao';
 cartao.innerHTML =  `
-
+<div class="conteudo-cartao">
+    <h3>categoria</h3>
+    <div class="pergunta-cartao">
+<p>pergunta</p>
+    </div>
+    <div class="resposta-cartao">
+<p>resposta</p>
+    </div>
+</div>
 `
 }
