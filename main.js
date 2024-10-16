28b03b8ccd003eed2d2560da7b8fa03eae175b1a
silvia-ramos10 <silviasrmc@gmail.com>
2024-10-16 11:08:16 -0300

    lç

--- main.js
+++ main.js
@@ -4,12 +4,12 @@ let cartao = document.createElement('article');
 cartao.className = 'cartao';
 cartao.innerHTML =  `
 <div class="conteudo-cartao">
-    <h3>categoria</h3>
+    <h3>${categoria}</h3>
     <div class="pergunta-cartao">
-<p>pergunta</p>
+<p>${pergunta}</p>
     </div>
     <div class="resposta-cartao">
-<p>resposta</p>
+<p>${resposta}</p>
     </div>
 </div>
 `
