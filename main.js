2165045536a94f68b8dc0ba26ef697ff4de76b0b
silvia-ramos10 <silviasrmc@gmail.com>
2024-10-16 11:56:23 -0300

    gf

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ cartao.className = 'cartao';
 cartao.innerHTML =  `
 <div class="conteudo-cartao">
     <h3>${categoria}</h3>
-    <div class="conteudo-cartaoconteudo-cartao">
+    <div class="pergunta-cartao">
 <p>${pergunta}</p>
     </div>
     <div class="resposta-cartao">
