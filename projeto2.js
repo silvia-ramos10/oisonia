1f656c815cf585f1ab8f6b963e47db1ce95392bb
silvia-ramos10 <silviasrmc@gmail.com>
2024-10-29 08:21:03 -0300

    h

--- projeto2.js
+++ projeto2.js
@@ -3,7 +3,7 @@ function criaCartao(categoria,pergunta,resposta){
     let cartao = document.createElement('article');
     cartao.className = 'cartao';
     cartao.innerHTML =  `
-    <div class="conteudo-cartao">
+    <div class="conteudo-cartao" onclick="mostraResposta (this)">
         <h3>${categoria}</h3>
         <div class="pergunta-cartao">
     <p>${pergunta}</p>
@@ -14,4 +14,14 @@ function criaCartao(categoria,pergunta,resposta){
     </div>
     `
     container.appendChild(cartao);
+    }
+
+    function mostraResposta (card){
+        const resposta = card.querySelector('.resposta-cartao');
+        if(resposta.style.display === 'none'){
+resposta.style.display = 'block';
+        }
+        else{
+resposta.style.display = 'none';
+        }
     }
