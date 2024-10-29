f60e58d32bd788172bf7918915a1ca8bb7bae6ed
silvia-ramos10 <silviasrmc@gmail.com>
2024-10-29 08:26:21 -0300

    ;

--- projeto2.js
+++ projeto2.js
@@ -18,7 +18,7 @@ function criaCartao(categoria,pergunta,resposta){
 
     function mostraResposta (card){
         const resposta = card.querySelector('.resposta-cartao');
-        if(resposta.style.display === 'none'){
+        if(resposta.style.display === 'none') {
 resposta.style.display = 'block';
         }
         else{
